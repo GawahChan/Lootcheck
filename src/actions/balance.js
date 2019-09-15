7cb30dfce9963bbe1eb281e513b6f5c9651e168c
Gawah Chan <gawahchan@gmail.com>
2019-09-15 12:52:01 +0100

    Added Deposit and Withdraw methods with redux

--- src/actions/balance.js
+++ src/actions/balance.js
@@ -1,9 +1,23 @@
 import * as constants from './constants'
 
-// action creator
+// action creators
 export const setBalance = (balance) => {
     return {
         type: constants.SET_BALANCE,
         balance
+    };
+}
+
+export const deposit = (deposit) => {
+    return {
+        type: constants.DEPOSIT,
+        deposit
+    };
+}
+
+export const withdraw = (withdraw) => {
+    return {
+        type: constants.WITHDRAW,
+        withdraw
     };
 }
