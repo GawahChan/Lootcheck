0d81dcc314e6d570c1971608fd93820000368cb4
Gawah Chan <gawahchan@gmail.com>
2019-09-15 16:11:47 +0100

    Created local state to handle local wallet balance

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -14,4 +14,20 @@ describe('Wallet', () => {
     it('displays the balance from props', () => {
         expect(wallet.find('.balance').text()).toEqual('Wallet balance: 20');
     });
+
+    it('creates input to deposit or withdraw from balance', () => {
+        expect(wallet.find('.input-wallet').exists()).toBe(true);
+    });
+
+    describe('when user types into wallet input', () => {
+        const userBalance = '25';
+
+        beforeEach(() => {
+            wallet.find('.input-wallet').simulate('change', { target: { value: userBalance }})
+        });
+
+        it('updates the local wallet balance in `state` and converts it to a number', () => {
+            expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+        });
+    })
 });
